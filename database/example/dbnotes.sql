d8f67b99061e5c661ecbf3e2385b8959d024fecd
Crist-afk <cristgarciapasaribu@gmail.com>
2025-12-17 21:36:25 +0700

    database users

--- database/example/dbnotes.sql
+++ database/example/dbnotes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2025 at 04:57 PM
+-- Generation Time: Dec 17, 2025 at 03:33 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
--- database/example/dbusers.sql
+++ database/example/dbusers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2025 at 04:57 PM
+-- Generation Time: Dec 17, 2025 at 03:33 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -41,13 +41,11 @@ CREATE TABLE `tbusers` (
 --
 
 INSERT INTO `tbusers` (`id`, `name`, `nim`, `email`, `password`, `programStudi`) VALUES
-(2101, 'Demo User', '01', 'demo@email.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Teknik Informatika'),
-(2105, 'Takanashi Hoshino', '2006', 'hoshinotakanashi@buruaka.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'Teknik Informatika'),
-(2107, 'dora 123', '1234567890', 'dora@gmail.com', '$2y$10$5QclJZJQD34Mvpf7VF3lgulD2dRz4QwFgusXKO.m1thf8AtwgZSzu', 'informatika'),
-(2108, 'Crist Garcia Pasaribu', '3312501041', 'crist@gmail.com', '$2y$10$UodgS2t3nSa1e18ZntSzHug/qmePTChZvqEddbYznlnQnyGQQbfKa', 'Teknik Informatika'),
-(2109, 'test akun', '1234567899', '123@gmal.com', '$2y$10$dGckwzk/voqITUFk9IRqmu63pWfXqKM.WfN/LAT6uFUehj7JlinLW', 'informatika'),
-(2110, 'Cahyati Lamona Sitohang', '3312501040', 'cahyati@email.com', '$2y$10$bPj.M30iwcltStvLH/JqTOZbtQYxm9iFq48AjAF4WDT7xu8BbjwiK', 'informatika'),
-(2111, 'Fazri Rahman', '3312501038', 'fazri@gmail.com', '$2y$10$diptF/r/Oy2UsgdvPJZ3UekhEx/rJxPTcQZRxSbDKowdY4G5fTFnG', 'informatika');
+(2112, 'halo disana', '123456789100', '123@gmail.com', '88888888', 'Teknik informatika'),
+(2113, 'kue sus', '1234567891', 'sus@gmail.com', 'kuesus123', 'Teknik Informatika'),
+(2114, 'Crist Garcia Pasaribu', '3312501041', 'crist@gmail.com', 'crist123', 'Teknik Informatika'),
+(2115, 'Cahyati Lamona Sitohang', '3312501040', 'cahyati@gmail.com', 'cahyati123', 'Teknik Informatika'),
+(2116, 'Fazri Rahman', '3312501038', 'fazri@gmail.com', 'fazri123', 'Teknik informatika');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +67,7 @@ ALTER TABLE `tbusers`
 -- AUTO_INCREMENT for table `tbusers`
 --
 ALTER TABLE `tbusers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2112;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2117;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
