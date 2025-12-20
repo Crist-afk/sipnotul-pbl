9242efcb4cfeb94b384e2f43ef0d94b1df38e47b
cahyati lamona sitohang <cahyatisitohang@gmail.com>
2025-12-21 03:15:25 +0700

    hapus file

--- php/conn_db_users.php
+++ php/conn_db_users.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = ""; // Kosongkan jika pakai XAMPP default
-$db   = "sipnotul"; // Pastikan nama database sesuai gambar Anda (dbusers)
+$db   = "sipnotul"; // Pastikan nama database sesuai gambar Anda (sipnotul)
 
 $conn_db_users = mysqli_connect($host, $user, $pass, $db);
 
