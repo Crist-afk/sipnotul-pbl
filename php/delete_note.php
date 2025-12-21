c62d405ad63b9385dd5c59754149ae87460b64d1
cahyati sitohang <cahyatisitohang@gmail.com>
2025-12-22 01:41:54 +0700

    perbaikan file

--- php/delete_note.php
+++ php/delete_note.php
@@ -7,12 +7,13 @@ $data = json_decode(file_get_contents("php://input"), true);
 if (isset($data['id'])) {
     $id = mysqli_real_escape_string($conn_db_notes, $data['id']);
     
-    // Hapus data
-    $query = "DELETE FROM tbNotesData WHERE idNotes = '$id'";
+    // PERBAIKAN: Nama tabel disesuaikan dengan SQL Dump (tbnotes_data)
+    $query = "DELETE FROM tbnotes_data WHERE idNotes = '$id'";
     
     if (mysqli_query($conn_db_notes, $query)) {
         echo json_encode(['status' => 'success']);
     } else {
+        // Jika query gagal, PHP akan mengirimkan pesan error MySQL yang spesifik
         echo json_encode(['status' => 'error', 'message' => mysqli_error($conn_db_notes)]);
     }
 } else {
